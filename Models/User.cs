25f8aa08fe8699b0364b2d90edc6c37be5e59116
recurzes <donlancelotknight123@gmail.com>
2025-12-06 17:13:59 +0800

    refactor: update `User` model properties to improve accessibility and data types
    - Changed `UserId` property from `protected` to `public`.
    - Updated `IsActive` property type from `int` to `bool`.

--- Models/User.cs
+++ Models/User.cs
@@ -2,11 +2,11 @@
 {
     public class User: Person
     {
-        protected int UserId { get; set; }
+        public int UserId { get; set; }
 
         public string UserName { get; set; }
         public string PasswordHash { get; set; }
         public int RoleId { get; set; }
-        public int IsActive { get; set; }
+        public bool IsActive { get; set; }
     }
 }
