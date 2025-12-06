bde9c9bec302e548849e039532b11713cb382182
recurzes <donlancelotknight123@gmail.com>
2025-12-06 17:06:05 +0800

    feat: add `Something` class with basic Main method for testing

---
+++ Test/Something.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Vormas.Test
+{
+    public class Something
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Bitchass");
+        }
+    }
+}
