d5027d9f21355275b201cece251b0bee42c92d71
recurzes <donlancelotknight123@gmail.com>
2025-12-06 17:13:45 +0800

    refactor: make `Person` class non-abstract in `Models`

--- Models/Person.cs
+++ Models/Person.cs
@@ -2,7 +2,7 @@
 
 namespace Vormas.Models
 {
-    public abstract class Person
+    public class Person
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
