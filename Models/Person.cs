db4a3e1e308da53be72797b7771549bf23ccceee
recurzes <donlancelotknight123@gmail.com>
2025-12-08 14:32:15 +0800

    feat: add `BirthDate` property to `Person` model

--- Models/Person.cs
+++ Models/Person.cs
@@ -10,6 +10,8 @@ namespace Vormas.Models
         public string Email { get; set; }
         public string Phone { get; set; }
         
+        public DateTime BirthDate { get; set; }
+        
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
     }
