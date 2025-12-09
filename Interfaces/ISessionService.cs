c7adf5f45de1eff7109715edfe293d79eb205fa9
recurzes <donlancelotknight123@gmail.com>
2025-12-09 17:36:23 +0800

    feat: add ISessionService interface for session management

---
+++ Interfaces/ISessionService.cs
@@ -0,0 +1,11 @@
+﻿using Vormas.Models;
+namespace Vormas.Interfaces
+{
+    public interface ISessionService
+    {
+        User CurrentUser { get; }
+        void SetCurrentUser(User user);
+        void ClearSession();
+        bool IsAuthenticated { get; }
+    }
+}
