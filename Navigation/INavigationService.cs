3ebd2429b483d74c649a9f3cddf001435a1a6d6b
recurzes <donlancelotknight123@gmail.com>
2025-12-12 12:07:28 +0800

    feat: add INavigationService interface for navigation handling

---
+++ Navigation/INavigationService.cs
@@ -0,0 +1,9 @@
+﻿namespace Vormas.Navigation
+{
+    public interface INavigationService
+    {
+        void Navigate(string route, object parameter = null);
+        bool CanGoBack { get; }
+        void GoBack();
+    }
+}
