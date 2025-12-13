1015ae9091cc689b0fd54909b8ae6e1c66b3db2f
recurzes <donlancelotknight123@gmail.com>
2025-12-13 13:47:58 +0800

    feat: add Routes class for centralized route management

---
+++ Navigation/Routes.cs
@@ -0,0 +1,10 @@
+﻿namespace Vormas.Navigation
+{
+    public static class Routes
+    {
+        public const string UserLogin = "user_login";
+        public const string UserRegister = "user_register";
+        public const string AdminDashboard = "admin_dashboard";
+        public const string RentalAgentDashboard = "rental_agent_dashboard";
+    }
+}
