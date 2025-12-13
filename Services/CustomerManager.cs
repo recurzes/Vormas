0b67e60b1e061b65ca0c24a5ed30a76c13398fb3
recurzes <donlancelotknight123@gmail.com>
2025-12-13 22:46:59 +0800

    feat: add CustomerManager class with initial methods for customer operations

---
+++ Services/CustomerManager.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+using Vormas.Database;
+using Vormas.Interfaces;
+using Vormas.Models;
+
+namespace Vormas.Services
+{
+    public class CustomerManager: ICustomerManager
+    {
+        private readonly CustomerDbContext _repo;
+
+        public CustomerManager(CustomerDbContext repo)
+        {
+            _repo = repo;
+        }
+        
+        // Methods
+        public void CreateCustomer(Customer customer)
+        {
+            
+        }
+
+        public List<Customer> GetALlCustomers()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Customer GetCustomerById(int customerId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Customer GetCustomerByPhone(string phoneNumber)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Customer GetCustomerByEmail(string email)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int UpdateCustomer(Customer customer)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int DeleteCustomer(int customerId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool CustomerExists(int customerId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool IsPhoneNumberTaken(string phoneNumber)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool IsEmailTaken(string email)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Customer> SearchCustomers(string searchTerm)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Customer> GetCustomerByStatus()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int DeactivateCustomer(int customerId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int ActivateCustomer(int customerId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
