fa732a20d5371af89fe8bb1aba61d35ebaff4618
Anex666 <vonanex23@gmail.com>
2025-12-15 11:32:16 +0800

    IVehicleService

---
+++ Interfaces/IVehicleService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Vormas.Models;
+
+namespace Vormas.Interfaces
+{
+    public interface IVehicleService
+    {
+        void AddVehicle(Vehicle vehicle);
+        void UpdateVehicle(Vehicle vehicle);
+        void DeleteVehicle(int vehicleId);
+        List<Vehicle> GetAllVehicles();
+        Vehicle GetVehicleById(int vehicleId);
+        IEnumerable<string> GetVehicleCategories();
+        IEnumerable<string> GetVehicleStatuses();
+    }
+}
