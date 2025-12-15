a4ac94c9893427329e63f1088f65da5b54006559
Anex666 <vonanex23@gmail.com>
2025-12-15 11:28:39 +0800

    Model/Vehicle

---
+++ Models/Vehicle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using FxResources.System;
+namespace Vormas.Models
+{
+    public class Vehicle
+    {
+        public int VehicleId { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+        public string LicensePlate { get; set; }
+        public string VIN { get; set; }
+        public string Category { get; set; } 
+        public string Transmission { get; set; } 
+        public string FuelType { get; set; } 
+        public int SeatingCapacity { get; set; }
+        public decimal DailyRate { get; set; }
+        public string Status { get; set; }
+        public string ImagePath { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
