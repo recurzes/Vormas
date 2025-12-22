15015fcc80e24d63804299c75de310b66c6228de
Anex666 <vonanex23@gmail.com>
2025-12-22 15:49:51 +0800

    feat: add route for CustomerForm in navigation service

--- Form1.cs
+++ Form1.cs
@@ -33,6 +33,7 @@ namespace Vormas
                 { Routes.UserRegister, () => new UserRegisterForm(_userManager, _authService) },
                 { Routes.RentalAgentDashboard, () => new RentalAgentDashboard(_sessionService) },
                 { Routes.AdminDashboard, () => new AdminDashboard(_sessionService) },
+                { Routes.Customers, () => new CustomerForm() }
             };
             
             _navigation = new NavigationService(contentHost, routes);
