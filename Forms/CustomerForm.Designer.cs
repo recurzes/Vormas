15dafebdbc19dca1b86c1ad2aa49a31017d1547d
recurzes <donlancelotknight123@gmail.com>
2025-12-23 15:44:43 +0800

    feat: enhance CustomerForm with vehicle input fields and data grid

--- Forms/CustomerForm.Designer.cs
+++ Forms/CustomerForm.Designer.cs
@@ -31,11 +31,405 @@ namespace Vormas.Forms
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dgvVehicles = new System.Windows.Forms.DataGridView();
+            this.pnlInputs = new System.Windows.Forms.Panel();
+            this.cmbStatus = new System.Windows.Forms.ComboBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.txtDailyRate = new System.Windows.Forms.TextBox();
+            this.lblDailyRate = new System.Windows.Forms.Label();
+            this.txtSeatingCapacity = new System.Windows.Forms.TextBox();
+            this.lblSeatingCapacity = new System.Windows.Forms.Label();
+            this.cmbFuelType = new System.Windows.Forms.ComboBox();
+            this.lblFuelType = new System.Windows.Forms.Label();
+            this.cmbTransmission = new System.Windows.Forms.ComboBox();
+            this.lblTransmission = new System.Windows.Forms.Label();
+            this.cmbCategory = new System.Windows.Forms.ComboBox();
+            this.lblCategory = new System.Windows.Forms.Label();
+            this.txtVin = new System.Windows.Forms.TextBox();
+            this.lblVin = new System.Windows.Forms.Label();
+            this.txtLicensePlate = new System.Windows.Forms.TextBox();
+            this.lblLicensePlate = new System.Windows.Forms.Label();
+            this.txtColor = new System.Windows.Forms.TextBox();
+            this.lblColor = new System.Windows.Forms.Label();
+            this.txtYear = new System.Windows.Forms.TextBox();
+            this.lblYear = new System.Windows.Forms.Label();
+            this.txtModel = new System.Windows.Forms.TextBox();
+            this.lblModel = new System.Windows.Forms.Label();
+            this.txtMake = new System.Windows.Forms.TextBox();
+            this.lblMake = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.pnlTop = new System.Windows.Forms.Panel();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.lblSearch = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvVehicles)).BeginInit();
+            this.pnlInputs.SuspendLayout();
+            this.pnlTop.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvVehicles
+            // 
+            this.dgvVehicles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvVehicles.Dock = System.Windows.Forms.DockStyle.Right;
+            this.dgvVehicles.Location = new System.Drawing.Point(350, 60);
+            this.dgvVehicles.Name = "dgvVehicles";
+            this.dgvVehicles.Size = new System.Drawing.Size(634, 480);
+            this.dgvVehicles.TabIndex = 4;
+            // 
+            // pnlInputs
+            // 
+            this.pnlInputs.AutoScroll = true;
+            this.pnlInputs.Controls.Add(this.cmbStatus);
+            this.pnlInputs.Controls.Add(this.lblStatus);
+            this.pnlInputs.Controls.Add(this.txtDailyRate);
+            this.pnlInputs.Controls.Add(this.lblDailyRate);
+            this.pnlInputs.Controls.Add(this.txtSeatingCapacity);
+            this.pnlInputs.Controls.Add(this.lblSeatingCapacity);
+            this.pnlInputs.Controls.Add(this.cmbFuelType);
+            this.pnlInputs.Controls.Add(this.lblFuelType);
+            this.pnlInputs.Controls.Add(this.cmbTransmission);
+            this.pnlInputs.Controls.Add(this.lblTransmission);
+            this.pnlInputs.Controls.Add(this.cmbCategory);
+            this.pnlInputs.Controls.Add(this.lblCategory);
+            this.pnlInputs.Controls.Add(this.txtVin);
+            this.pnlInputs.Controls.Add(this.lblVin);
+            this.pnlInputs.Controls.Add(this.txtLicensePlate);
+            this.pnlInputs.Controls.Add(this.lblLicensePlate);
+            this.pnlInputs.Controls.Add(this.txtColor);
+            this.pnlInputs.Controls.Add(this.lblColor);
+            this.pnlInputs.Controls.Add(this.txtYear);
+            this.pnlInputs.Controls.Add(this.lblYear);
+            this.pnlInputs.Controls.Add(this.txtModel);
+            this.pnlInputs.Controls.Add(this.lblModel);
+            this.pnlInputs.Controls.Add(this.txtMake);
+            this.pnlInputs.Controls.Add(this.lblMake);
+            this.pnlInputs.Controls.Add(this.btnClear);
+            this.pnlInputs.Controls.Add(this.btnDelete);
+            this.pnlInputs.Controls.Add(this.btnSave);
+            this.pnlInputs.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlInputs.Location = new System.Drawing.Point(0, 60);
+            this.pnlInputs.Name = "pnlInputs";
+            this.pnlInputs.Size = new System.Drawing.Size(340, 480);
+            this.pnlInputs.TabIndex = 3;
+            // 
+            // cmbStatus
+            // 
+            this.cmbStatus.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbStatus.FormattingEnabled = true;
+            this.cmbStatus.Location = new System.Drawing.Point(120, 250);
+            this.cmbStatus.Name = "cmbStatus";
+            this.cmbStatus.Size = new System.Drawing.Size(200, 21);
+            this.cmbStatus.TabIndex = 22;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(10, 253);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(40, 13);
+            this.lblStatus.TabIndex = 21;
+            this.lblStatus.Text = "Status:";
+            // 
+            // txtDailyRate
+            // 
+            this.txtDailyRate.Location = new System.Drawing.Point(120, 226);
+            this.txtDailyRate.Name = "txtDailyRate";
+            this.txtDailyRate.Size = new System.Drawing.Size(200, 20);
+            this.txtDailyRate.TabIndex = 20;
+            // 
+            // lblDailyRate
+            // 
+            this.lblDailyRate.AutoSize = true;
+            this.lblDailyRate.Location = new System.Drawing.Point(10, 229);
+            this.lblDailyRate.Name = "lblDailyRate";
+            this.lblDailyRate.Size = new System.Drawing.Size(59, 13);
+            this.lblDailyRate.TabIndex = 19;
+            this.lblDailyRate.Text = "Daily Rate:";
+            // 
+            // txtSeatingCapacity
+            // 
+            this.txtSeatingCapacity.Location = new System.Drawing.Point(120, 202);
+            this.txtSeatingCapacity.Name = "txtSeatingCapacity";
+            this.txtSeatingCapacity.Size = new System.Drawing.Size(200, 20);
+            this.txtSeatingCapacity.TabIndex = 18;
+            // 
+            // lblSeatingCapacity
+            // 
+            this.lblSeatingCapacity.AutoSize = true;
+            this.lblSeatingCapacity.Location = new System.Drawing.Point(10, 205);
+            this.lblSeatingCapacity.Name = "lblSeatingCapacity";
+            this.lblSeatingCapacity.Size = new System.Drawing.Size(51, 13);
+            this.lblSeatingCapacity.TabIndex = 17;
+            this.lblSeatingCapacity.Text = "Capacity:";
+            // 
+            // cmbFuelType
+            // 
+            this.cmbFuelType.FormattingEnabled = true;
+            this.cmbFuelType.Items.AddRange(new object[] { "Gasoline", "Diesel", "Electric", "Hybrid" });
+            this.cmbFuelType.Location = new System.Drawing.Point(120, 177);
+            this.cmbFuelType.Name = "cmbFuelType";
+            this.cmbFuelType.Size = new System.Drawing.Size(200, 21);
+            this.cmbFuelType.TabIndex = 16;
+            // 
+            // lblFuelType
+            // 
+            this.lblFuelType.AutoSize = true;
+            this.lblFuelType.Location = new System.Drawing.Point(10, 180);
+            this.lblFuelType.Name = "lblFuelType";
+            this.lblFuelType.Size = new System.Drawing.Size(57, 13);
+            this.lblFuelType.TabIndex = 15;
+            this.lblFuelType.Text = "Fuel Type:";
+            // 
+            // cmbTransmission
+            // 
+            this.cmbTransmission.FormattingEnabled = true;
+            this.cmbTransmission.Items.AddRange(new object[] { "Automatic", "Manual" });
+            this.cmbTransmission.Location = new System.Drawing.Point(120, 153);
+            this.cmbTransmission.Name = "cmbTransmission";
+            this.cmbTransmission.Size = new System.Drawing.Size(200, 21);
+            this.cmbTransmission.TabIndex = 14;
+            // 
+            // lblTransmission
+            // 
+            this.lblTransmission.AutoSize = true;
+            this.lblTransmission.Location = new System.Drawing.Point(10, 156);
+            this.lblTransmission.Name = "lblTransmission";
+            this.lblTransmission.Size = new System.Drawing.Size(71, 13);
+            this.lblTransmission.TabIndex = 13;
+            this.lblTransmission.Text = "Transmission:";
+            // 
+            // cmbCategory
+            // 
+            this.cmbCategory.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbCategory.FormattingEnabled = true;
+            this.cmbCategory.Items.AddRange(new object[] { "Hatchback ", "Sedan", "SUV ", "Pickup ", "Van/Minibus " });
+            this.cmbCategory.Location = new System.Drawing.Point(120, 129);
+            this.cmbCategory.Name = "cmbCategory";
+            this.cmbCategory.Size = new System.Drawing.Size(200, 21);
+            this.cmbCategory.TabIndex = 12;
+            // 
+            // lblCategory
+            // 
+            this.lblCategory.AutoSize = true;
+            this.lblCategory.Location = new System.Drawing.Point(10, 132);
+            this.lblCategory.Name = "lblCategory";
+            this.lblCategory.Size = new System.Drawing.Size(52, 13);
+            this.lblCategory.TabIndex = 11;
+            this.lblCategory.Text = "Category:";
+            // 
+            // txtVin
+            // 
+            this.txtVin.Location = new System.Drawing.Point(120, 105);
+            this.txtVin.Name = "txtVin";
+            this.txtVin.Size = new System.Drawing.Size(200, 20);
+            this.txtVin.TabIndex = 10;
+            // 
+            // lblVin
+            // 
+            this.lblVin.AutoSize = true;
+            this.lblVin.Location = new System.Drawing.Point(10, 108);
+            this.lblVin.Name = "lblVin";
+            this.lblVin.Size = new System.Drawing.Size(28, 13);
+            this.lblVin.TabIndex = 9;
+            this.lblVin.Text = "VIN:";
+            // 
+            // txtLicensePlate
+            // 
+            this.txtLicensePlate.Location = new System.Drawing.Point(120, 81);
+            this.txtLicensePlate.Name = "txtLicensePlate";
+            this.txtLicensePlate.Size = new System.Drawing.Size(200, 20);
+            this.txtLicensePlate.TabIndex = 8;
+            // 
+            // lblLicensePlate
+            // 
+            this.lblLicensePlate.AutoSize = true;
+            this.lblLicensePlate.Location = new System.Drawing.Point(10, 84);
+            this.lblLicensePlate.Name = "lblLicensePlate";
+            this.lblLicensePlate.Size = new System.Drawing.Size(74, 13);
+            this.lblLicensePlate.TabIndex = 7;
+            this.lblLicensePlate.Text = "License Plate:";
+            // 
+            // txtColor
+            // 
+            this.txtColor.Location = new System.Drawing.Point(230, 57);
+            this.txtColor.Name = "txtColor";
+            this.txtColor.Size = new System.Drawing.Size(90, 20);
+            this.txtColor.TabIndex = 6;
+            // 
+            // lblColor
+            // 
+            this.lblColor.AutoSize = true;
+            this.lblColor.Location = new System.Drawing.Point(190, 60);
+            this.lblColor.Name = "lblColor";
+            this.lblColor.Size = new System.Drawing.Size(34, 13);
+            this.lblColor.TabIndex = 5;
+            this.lblColor.Text = "Color:";
+            // 
+            // txtYear
+            // 
+            this.txtYear.Location = new System.Drawing.Point(120, 57);
+            this.txtYear.Name = "txtYear";
+            this.txtYear.Size = new System.Drawing.Size(60, 20);
+            this.txtYear.TabIndex = 4;
+            // 
+            // lblYear
+            // 
+            this.lblYear.AutoSize = true;
+            this.lblYear.Location = new System.Drawing.Point(10, 60);
+            this.lblYear.Name = "lblYear";
+            this.lblYear.Size = new System.Drawing.Size(32, 13);
+            this.lblYear.TabIndex = 3;
+            this.lblYear.Text = "Year:";
+            // 
+            // txtModel
+            // 
+            this.txtModel.Location = new System.Drawing.Point(120, 33);
+            this.txtModel.Name = "txtModel";
+            this.txtModel.Size = new System.Drawing.Size(200, 20);
+            this.txtModel.TabIndex = 2;
+            // 
+            // lblModel
+            // 
+            this.lblModel.AutoSize = true;
+            this.lblModel.Location = new System.Drawing.Point(10, 36);
+            this.lblModel.Name = "lblModel";
+            this.lblModel.Size = new System.Drawing.Size(39, 13);
+            this.lblModel.TabIndex = 1;
+            this.lblModel.Text = "Model:";
+            // 
+            // txtMake
+            // 
+            this.txtMake.Location = new System.Drawing.Point(120, 9);
+            this.txtMake.Name = "txtMake";
+            this.txtMake.Size = new System.Drawing.Size(200, 20);
+            this.txtMake.TabIndex = 0;
+            // 
+            // lblMake
+            // 
+            this.lblMake.AutoSize = true;
+            this.lblMake.Location = new System.Drawing.Point(10, 12);
+            this.lblMake.Name = "lblMake";
+            this.lblMake.Size = new System.Drawing.Size(37, 13);
+            this.lblMake.TabIndex = 0;
+            this.lblMake.Text = "Make:";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(245, 435);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 30);
+            this.btnClear.TabIndex = 27;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.IndianRed;
+            this.btnDelete.Location = new System.Drawing.Point(135, 435);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 30);
+            this.btnDelete.TabIndex = 26;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnSave.Location = new System.Drawing.Point(25, 435);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 30);
+            this.btnSave.TabIndex = 25;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = false;
+            // 
+            // pnlTop
+            // 
+            this.pnlTop.Controls.Add(this.btnSearch);
+            this.pnlTop.Controls.Add(this.txtSearch);
+            this.pnlTop.Controls.Add(this.lblSearch);
+            this.pnlTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlTop.Location = new System.Drawing.Point(0, 0);
+            this.pnlTop.Name = "pnlTop";
+            this.pnlTop.Size = new System.Drawing.Size(984, 60);
+            this.pnlTop.TabIndex = 5;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(340, 18);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(60, 20);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(260, 20);
+            this.txtSearch.TabIndex = 1;
+            // 
+            // lblSearch
+            // 
+            this.lblSearch.AutoSize = true;
+            this.lblSearch.Location = new System.Drawing.Point(10, 23);
+            this.lblSearch.Name = "lblSearch";
+            this.lblSearch.Size = new System.Drawing.Size(44, 13);
+            this.lblSearch.TabIndex = 0;
+            this.lblSearch.Text = "Search:";
+            // 
+            // CustomerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "CustomerForm";
+            this.Controls.Add(this.dgvVehicles);
+            this.Controls.Add(this.pnlInputs);
+            this.Controls.Add(this.pnlTop);
+            this.Name = "CustomerForm";
+            this.Size = new System.Drawing.Size(984, 540);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvVehicles)).EndInit();
+            this.pnlInputs.ResumeLayout(false);
+            this.pnlInputs.PerformLayout();
+            this.pnlTop.ResumeLayout(false);
+            this.pnlTop.PerformLayout();
+            this.ResumeLayout(false);
         }
+
+        private System.Windows.Forms.DataGridView dgvVehicles;
+        private System.Windows.Forms.Panel pnlInputs;
+        private System.Windows.Forms.ComboBox cmbStatus;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.TextBox txtDailyRate;
+        private System.Windows.Forms.Label lblDailyRate;
+        private System.Windows.Forms.TextBox txtSeatingCapacity;
+        private System.Windows.Forms.Label lblSeatingCapacity;
+        private System.Windows.Forms.ComboBox cmbFuelType;
+        private System.Windows.Forms.Label lblFuelType;
+        private System.Windows.Forms.ComboBox cmbTransmission;
+        private System.Windows.Forms.Label lblTransmission;
+        private System.Windows.Forms.ComboBox cmbCategory;
+        private System.Windows.Forms.Label lblCategory;
+        private System.Windows.Forms.TextBox txtVin;
+        private System.Windows.Forms.Label lblVin;
+        private System.Windows.Forms.TextBox txtLicensePlate;
+        private System.Windows.Forms.Label lblLicensePlate;
+        private System.Windows.Forms.TextBox txtColor;
+        private System.Windows.Forms.Label lblColor;
+        private System.Windows.Forms.TextBox txtYear;
+        private System.Windows.Forms.Label lblYear;
+        private System.Windows.Forms.TextBox txtModel;
+        private System.Windows.Forms.Label lblModel;
+        private System.Windows.Forms.TextBox txtMake;
+        private System.Windows.Forms.Label lblMake;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Panel pnlTop;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label lblSearch;
 
         #endregion
     }
