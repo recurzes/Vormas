a6ba541735b80d10cc2ca1b9e5ab4d7581e680e9
recurzes <donlancelotknight123@gmail.com>
2025-12-23 16:11:21 +0800

    feat: add VehicleCode property and update Odometer in Vehicle class; resize Form1 layout

--- Models/Vehicle.cs
+++ Models/Vehicle.cs
@@ -5,6 +5,7 @@ namespace Vormas.Models
     public class Vehicle
     {
         public int VehicleId { get; set; }
+        public string VehicleCode { get; set; }
         public string Make { get; set; }
         public string Model { get; set; }
         public int Year { get; set; }
@@ -15,7 +16,7 @@ namespace Vormas.Models
         public string Transmission { get; set; } 
         public string FuelType { get; set; } 
         public int SeatingCapacity { get; set; }
-        public decimal DailyRate { get; set; }
+        public int Odometer { get; set; }
         public string Status { get; set; }
         public string ImagePath { get; set; }
         public DateTime CreatedAt { get; set; }
