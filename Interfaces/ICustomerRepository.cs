92e55d085b9d8e244adfe7d93f307fd879cd6655
recurzes <donlancelotknight123@gmail.com>
2025-12-23 19:04:59 +0800

    feat: Add ICustomerRepository and ICustomerService interfaces for customer management

---
+++ Interfaces/ICustomerRepository.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Vormas.Models;
+
+namespace Vormas.Interfaces
+{
+    public interface ICustomerRepository
+    {
+        void CreateCustomer(Customer customer);
+        List<Customer> GetAllCustomers();
+        Customer GetCustomerById(int customerId);
+        Customer GetCustomerByPhone(string phoneNumber);
+        Customer GetCustomerByEmail(string email);
+        
+        int UpdateCustomer(Customer customer);
+        int DeleteCustomer(int customerId);
+        bool CustomerExists(int customerId);
+        bool IsPhoneNumberTaken(string phoneNumber);
+        bool IsEmailTaken(string email);
+        
+        // Search and Filtering
+        List<Customer> SearchCustomers(string searchTerm);
+        List<Customer> GetCustomerByStatus();
+
+        int DeactivateCustomer(int customerId);
+        int ActivateCustomer(int customerId);
+
+        DriverLicense GetLicenseByCustomerId(int customerId);
+        int UpsertLicense(DriverLicense license);
+    }
+}
