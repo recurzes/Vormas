de3253c6b7152c8788da7f1a9a349342c537eb5b
Anex666 <vonanex23@gmail.com>
2026-01-06 20:04:39 +0800

    feat: add IReservationRepository interface for vehicle reservation management

---
+++ Interfaces/IReservationRepo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Vormas.Models;
+
+namespace Vormas.Interfaces
+{
+    public interface IReservationRepository
+    {
+        bool CheckVehicleAvailability(int vehicleId, DateTime startDate, DateTime endDate);
+        void CreateReservation(Reservation reservation);
+        // Future extensibility methods (optional)
+        // List<Reservation> GetAllReservations();
+        // Reservation GetReservationById(int reservationId);
+    }
+}
